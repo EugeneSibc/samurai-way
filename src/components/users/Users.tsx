9c1c6c0da0d44284b1c8d0a7223593b096b41e8a
eugeneman <evgensbk9@gmail.com>
2024-03-25 19:54:13 +0700

    instal styled-component

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { InitialUsersState } from '../../redux/users-reducer';
+import styled from "styled-components";
 
-const Users = () => {
+
+const Users = (props: InitialUsersState) => {
     return (
         <div>
-            
+            {props.users.map(u => <div key={u.id}>            
+                <span>
+                    <div>
+                    </div>
+                </span>
+                <span>
+                    <div>
+
+                    </div>
+                </span>
+            </div>
+            )}
         </div>
     );
 };
 
 export default Users;
+
+const StyledUsersPage = styled.img`
+    
+`
