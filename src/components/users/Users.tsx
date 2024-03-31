f1787faddf522d8abae40a6cdde41b4f9997b23d
eugeneman <evgensbk9@gmail.com>
2024-03-31 20:33:49 +0700

    add function getUsers

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -7,14 +7,17 @@ import userPhoto from '../../assets/png-clipart-computer-icons-avatar-icon-desig
 
 type UsersProps = InitialUsersState & MapDispatchUsers
 const Users = (props: UsersProps) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(u => <StyledUsersPage key={u.id}>
                 <span>
                     <div>
